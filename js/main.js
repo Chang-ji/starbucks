e204edd8fa526794c03343b5a0d723201fdc474f
Chang-ji <75286090+Chang-ji@users.noreply.github.com>
2021-08-23 23:03:39 +0900

    공지사항 html css 작성

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
 const searchEl = document.querySelector('.search');
 const searchInputEl = searchEl.querySelector('input');
 
-searchEl.addEventListener('click', function(){
+searchEl.addEventListener('click', function () {
   searchInputEl.focus();
 });
 
-searchInputEl.addEventListener('focus', function(){
+searchInputEl.addEventListener('focus', function () {
   searchEl.classList.add('focused');
-  searchInputEl.setAttribute('placeholder','통합검색');
+  searchInputEl.setAttribute('placeholder', '통합검색');
 })
 
-searchInputEl.addEventListener('blur', function(){
+searchInputEl.addEventListener('blur', function () {
   searchEl.classList.remove('focused');
-  searchInputEl.setAttribute('placeholder','');
+  searchInputEl.setAttribute('placeholder', '');
 })
 
 
 const badgeEl = document.querySelector('header .badges');
 
-window.addEventListener('scroll',_.throttle(function() {
-  if(window.scrollY > 500) {
+window.addEventListener('scroll', _.throttle(function () {
+  if (window.scrollY > 500) {
     // 배지 숨기기
     //gsap.to(요소, 지속시간, 옵션);
     gsap.to(badgeEl, .6, {
@@ -33,5 +33,14 @@ window.addEventListener('scroll',_.throttle(function() {
       display: 'block'
     })
   }
-},300));
+}, 300));
 //_.throtle(함수, 시간)
+
+const fadeEls = document.querySelectorAll('.visual .fade-in');
+fadeEls.forEach(function (fadeEl, index) {
+  //gsap.to(요소, 지속시간, 옵션);
+  gsap.to(fadeEl, 1, {
+    delay: (index + 1) * .7, // 0.7초, 1.4초, 2.1초 2.7초
+    opacity: 1
+  });
+});
